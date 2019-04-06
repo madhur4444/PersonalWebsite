ed015c2ef166aaa2213a0bd6890720ce77a67ef9
Madhur Kumar Sharma <mksmks44444444@gmail.com>
2019-04-05 22:03:30 -0400

    Changed App.js, added intro and links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './css/reset.css'
+import './css/white-black.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        	<main>
+            <div className="intro">Hello, I'm Madhur!</div>
+            {/* add sub line later  */}
+              <div className="tagline">Spark DEV | Blockchain Fanatic | Linux Hacker | Yeahh</div>
+             {/* Fonts are here https://fontawesome.com/icons?d=gallery&s=brands */}
+                  <div className="icons-social">
+                        <a target="_blank" href="https://github.com/madhur4444"><i className="fa fa-github"></i></a>
+                        <a target="_blank" href="https://twitter.com/madhur4444"><i className="fa fa-twitter"></i></a>
+                        <a target="_blank" href="https://www.linkedin.com/in/madhurkumarsharma/"><i className="fa fa-linkedin"></i></a>
+                        {/* add link to devpost blogs and other projects, see louisgv.ga, add resume*/}
+                  </div>
+	        </main>
       </div>
     );
   }
