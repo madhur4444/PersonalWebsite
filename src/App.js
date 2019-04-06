286507917cb514a1037633087f372fd0398da1ac
Madhur Kumar Sharma <mksmks44444444@gmail.com>
2019-04-05 23:15:28 -0400

    added rel to links for blank targets

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ class App extends Component {
               <div className="tagline">Spark DEV | Blockchain Fanatic | Linux Hacker | Yeahh</div>
              {/* Fonts are here https://fontawesome.com/icons?d=gallery&s=brands */}
                   <div className="icons-social">
-                        <a target="_blank" href="https://github.com/madhur4444"><i className="fa fa-github"></i></a>
-                        <a target="_blank" href="https://twitter.com/madhur4444"><i className="fa fa-twitter"></i></a>
-                        <a target="_blank" href="https://www.linkedin.com/in/madhurkumarsharma/"><i className="fa fa-linkedin"></i></a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/madhur4444"><i className="fa fa-github"></i></a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/madhur4444"><i className="fa fa-twitter"></i></a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/madhurkumarsharma/"><i className="fa fa-linkedin"></i></a>
                         {/* add link to devpost blogs and other projects, see louisgv.ga, add resume*/}
                   </div>
 	        </main>
