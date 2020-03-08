3a277a8eca931e26c42f41cd04e6c9b161466523
Madhur Kumar Sharma <mk3sharm@edu.uwaterloo.ca>
2020-03-08 13:51:54 -0400

    added devpost

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ class App extends Component {
         	<main>
             <div className="intro">Hello, I'm Madhur! (Aka MKS)</div>
             {/* add sub line later  */}
-              <div className="tagline">Spark DEV | Blockchain Fanatic | Linux Hacker | Yeahh</div>
-             {/* Fonts are here https://fontawesome.com/icons?d=gallery&s=brands */}
+              <div className="tagline">Spark DEV | DeVOps | Blockchain Fanatic | Linux Hacker </div>
+             
                   <div className="icons-social">
                         <a target="_blank" rel="noopener noreferrer" href="https://github.com/madhur4444"><i className="fa fa-github"></i></a>
                         <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/madhur4444"><i className="fa fa-twitter"></i></a>
                         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/madhurkumarsharma/"><i className="fa fa-linkedin"></i></a>
-                        {/* add link to devpost blogs and other projects, see louisgv.ga, add resume*/}
+	    		<a target="_blank" rel="noopener noreferrer" href="https://devpost.com/madhur44444444"><i className="fa fa-devpost"></i></a>
                   </div>
 	        </main>
       </div>
