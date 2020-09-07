39d1c17e92a8ea4b215a8c57186279bc99f60421
madhur4444 <mksmks44444444@gmail.com>
2020-09-08 04:03:09 +0530

    add current involvements and projects

--- src/App.js
+++ src/App.js
@@ -17,8 +17,19 @@ class App extends Component {
                         <a target="_blank" rel="noopener noreferrer" href="https://github.com/madhur4444"><i className="fa fa-github"></i></a>
                         <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/madhur4444"><i className="fa fa-twitter"></i></a>
                         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/madhurkumarsharma/"><i className="fa fa-linkedin"></i></a>
-	    		<a target="_blank" rel="noopener noreferrer" href="https://devpost.com/madhur44444444"><i className="fa fa-code"></i></a>
+	    		              <a target="_blank" rel="noopener noreferrer" href="https://devpost.com/madhur44444444"><i className="fa fa-code"></i></a>
                   </div>
+                  <div className="currentProjects"><p><b>Current Projects - </b> <br></br> <br></br>
+                  - Working with DESY skillchain team with Indian Government to implement decentralized education <br></br>
+                    system using Hyperledger Aries, decentralized identies, Verifiable Credentials, IPFS and Xamarin mobile agents. <br></br>
+                    Media Coverage (<a href="https://timesofindia.indiatimes.com/blogs/breaking-shackles/blockchain-technology-model-to-transform-indian-education-system/">Link to Times of India Article</a>) <br></br>
+                  - Working in MathCheck Research Group on SAT+CAS paradigm with Prof. Curtis Bright and Prof. Vijay Ganesh</p> 
+                  </div>
+
+                  <div className="currentProjects">
+                  <span>Website Last Updated on Sept 8, 2020</span>
+                  </div>
+
 	        </main>
       </div>
     );
