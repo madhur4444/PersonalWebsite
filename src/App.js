636bca1e62f97782c31e801071b602741e4c531e
Madhur Kumar Sharma <mk3sharm@edu.uwaterloo.ca>
2021-03-16 04:32:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,15 +19,12 @@ class App extends Component {
                         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/madhurkumarsharma/"><i className="fa fa-linkedin"></i></a>
 	    		              <a target="_blank" rel="noopener noreferrer" href="https://devpost.com/madhur44444444"><i className="fa fa-code"></i></a>
                   </div>
-                  <div className="currentProjects"><p><b>Current Projects - </b> <br></br> <br></br>
-                  - Working with DESY skillchain team with Indian Government to implement decentralized education <br></br>
-                    system using Hyperledger Aries, decentralized identies, Verifiable Credentials, IPFS and Xamarin mobile agents. <br></br>
-                    Media Coverage (<a href="https://timesofindia.indiatimes.com/blogs/breaking-shackles/blockchain-technology-model-to-transform-indian-education-system/">Link to Times of India Article</a>) <br></br>
-                  - Working in MathCheck Research Group on SAT+CAS paradigm with Prof. Curtis Bright and Prof. Vijay Ganesh</p> 
+                  <div className="currentProjects"><p><b>Current Updates - </b> <br></br> <br></br>
+                  - Working at Manulife Financial as DevOps Engineer<br></br>
                   </div>
 
                   <div className="currentProjects">
-                  <span>Website Last Updated on Sept 8, 2020</span>
+                  <span>Website Last Updated on March, 2021</span>
                   </div>
 
 	        </main>
